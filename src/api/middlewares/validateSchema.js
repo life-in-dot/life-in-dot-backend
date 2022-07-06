18f233b49fcb74d5b6653b8003cde57be9c537af
Taewan Lim <taewan.seoul@gmail.com>
2022-07-06 14:15:01 +0900

    ✨ feat: set up validateSchema and verifyToken middlewares

---
+++ src/api/middlewares/validateSchema.js
@@ -0,0 +1,15 @@
+const createError = require("http-errors");
+const logger = require("../../loaders/logger");
+
+module.exports = function validateSchema(schema, property) {
+  return (req, res, next) => {
+    const { error } = schema.validateSchema(req[property]);
+
+    if (error) {
+      next(createError(400));
+      logger.error(error);
+    }
+
+    next();
+  };
+};
