708ded807cba99c28004fb19252675541fd03a14
Taewan Lim <taewan.seoul@gmail.com>
2022-07-13 00:19:14 +0900

    ✨ feat: set up data endpoints

--- src/api/controllers/user.js
+++ src/api/controllers/user.js
@@ -1,6 +1,8 @@
 const createError = require("http-errors");
 const jwt = require("jsonwebtoken");
+const { startSession } = require("mongoose");
 
+const { User } = require("../../models/User");
 const { Journal } = require("../../models/Journal");
 
 const catchAsync = require("../../utils/asyncHandler");
@@ -8,8 +10,112 @@ const catchAsync = require("../../utils/asyncHandler");
 exports.getUserAllJournals = catchAsync(async (req, res, next) => {
   const { user_id } = req.params;
 
+  const userData = await User.findOne({ _id: user_id })
+    .populate("myJournals")
+    .lean();
+
+  const userJournals = userData.myJournals;
+
   return res.status(200).json({
     result: "ok",
-    data: [],
+    data: [...userJournals],
   });
+});
+
+exports.getUserJournal = catchAsync(async (req, res, next) => {
+  const { journal_id } = req.params;
+
+  const userJournal = await Journal.findOne({ _id: journal_id }).lean();
+
+  delete userJournal.__v;
+
+  return res.status(200).json({
+    result: "ok",
+    data: { ...userJournal },
+  });
+});
+
+exports.createJournal = catchAsync(async (req, res, next) => {
+  const { user_id } = req.params;
+  const { dateId, title, contents } = req.body;
+  const session = await startSession();
+  let newJournal;
+
+  await session.withTransaction(async () => {
+    const newJournalArr = await Journal.create(
+      [
+        {
+          dateId,
+          title,
+          contents,
+          contentsSize: contents.length,
+        },
+      ],
+      { session },
+    );
+
+    newJournal = newJournalArr[0].toObject();
+    delete newJournal.__v;
+
+    await User.updateOne(
+      { _id: user_id },
+      { $push: { myJournals: newJournal._id } },
+      { session },
+    );
+  });
+
+  session.endSession();
+
+  return res.status(200).json({
+    result: "ok",
+    data: { ...newJournal },
+  });
+});
+
+exports.updateJournal = catchAsync(async (req, res, next) => {
+  const { journal_id } = req.params;
+
+  await Journal.findByIdAndUpdate(
+    { _id: journal_id },
+    { ...req.body },
+    { new: true },
+  );
+
+  return res.status(200).json({
+    result: "ok",
+  });
+});
+
+exports.replaceJournal = catchAsync(async (req, res, next) => {
+  const { journal_id } = req.params;
+  const { contents } = req.body;
+  const lastSavedTime = Date.now();
+
+  await Journal.findOneAndReplace(
+    { _id: journal_id },
+    { ...req.body, contentsSize: contents.length, lastSavedTime },
+    { new: true },
+  );
+
+  return res.status(201).json({
+    result: "ok",
+  });
+});
+
+exports.deleteJournal = catchAsync(async (req, res, next) => {
+  const { user_id, journal_id } = req.params;
+  const session = await startSession();
+
+  await session.withTransaction(async () => {
+    await User.updateOne(
+      { _id: user_id },
+      { $pull: { myJournals: journal_id } },
+    );
+
+    await Journal.findByIdAndDelete({ _id: journal_id });
+  });
+
+  session.endSession();
+
+  return res.status(200).json({ result: "ok" });
 });
